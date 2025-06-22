a93439c99aa241596bea85be92e991434ba32345
Kian-Becera <becera.kian@gmail.com>
2025-06-22 14:26:11 +0800

    creating footer

---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+<section class="footer">
+  <div class="footer-wrapper">
+    <div class="footer-title">
+      <a href="#top">Cotamilla</a>
+      <p>Est. 2021</p>
+    </div>
+    <div class="footer-contact">
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/share/1EwTM1AnCS/" class="footer-logo">
+            <img src="/assets/images/cota-fb.png" alt="Facebook">
+          </a>
+          <p>Facebook</p>
+        </li>
+        <li>
+          <a href="" class="footer-logo">
+            <img src="/assets/images/cota-mail.png" alt="Mail">
+          </a>
+          <p>Email</p>
+        </li>
+        <li>
+          <a href="" class="footer-logo">
+            <img src="/assets/images/cota-phone.png" alt="Phone">
+          </a>
+          <p>Phone</p>
+        </li>
+        <li>
+          <a href="https://maps.app.goo.gl/1EvoZ7uvGG5AD5LEA" class="footer-logo">
+            <img src="/assets/images/cota-map.png" alt="Address">
+          </a>
+          <p>Address</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
